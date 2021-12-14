0aff2ccefeae39f8c8ac44de2cecef5873ce54bb
danyache <dchesakov@nes.ru>
2021-12-14 14:36:07 +0000

    load weights

---
+++ download_models.sh
@@ -0,0 +1,10 @@
+# load arcface
+wget -P ./arcface_model https://github.com/Danyache/sber-swap/releases/download/arcface/backbone.pth
+wget -P ./arcface_model https://github.com/Danyache/sber-swap/releases/download/arcface/iresnet.py
+
+# load landmarks detector
+wget -P ./insightface_func/models/antelope https://github.com/Danyache/sber-swap/releases/download/antelope/glintr100.onnx
+wget -P ./insightface_func/models/antelope https://github.com/Danyache/sber-swap/releases/download/antelope/scrfd_10g_bnkps.onnx
+
+# load model itself
+wget -P ./weights https://github.com/Danyache/sber-swap/releases/download/sber-swap-v1.0/G_0_035000_init_arch_arcface2.pth
