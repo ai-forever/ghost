48879cb14a12f6207160eaf2e483b8be41726297
AlexanderGroshev <Nnn.alg@yandex.ru>
2022-01-12 15:20:01 +0300

    minor fix (#8)
    * minor fix
    
    * Delete dora_short.mp4
    
    * Delete obama_short.mp4
    
    * Delete video5_mod.mp4
    
    * Delete efiop_short.mp4
    
    * Delete zak.jpg
    
    * Delete 3person.png
    
    * Delete 1.png
    
    * Delete 2.png
    
    * Delete 3.png
    
    * Delete elon2dora.mp4
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Update config_sr.py
    
    * Update preprocess_vgg.py
    
    Co-authored-by: danyache <dchesakov@nes.ru>

--- models/config_sr.py
+++ models/config_sr.py
@@ -9,42 +9,21 @@ class TestOptions(object):
     which_epoch = '10'
 
     aspect_ratio = 1.0
-    batchSize = 1
-    cache_filelist_read = True
-    cache_filelist_write = True
     checkpoints_dir = ''
-    coco_no_portraits = False
-    contain_dontcare_label = False
-
-    display_winsize = 256
-    how_many = sys.maxsize
-    how_many = 10
+    
     init_type = 'xavier'
     init_variance = 0.02
     isTrain = False
     is_test = True
-    label_nc = 3
-    output_nc = 3
     semantic_nc = 3
-    load_from_opt_file = False
-    max_dataset_size = sys.maxsize
     
     model = 'pix2pix'
-    nThreads = 0
     netG = 'lipspade'
     nef = 16
     ngf = 48 
-    no_flip = True
-    no_instance = True
-    no_pairing_check = False
-
-    norm_D = 'spectralinstance'
-    norm_E = 'spectralinstance'
+    
     norm_G = 'spectralspadesyncbatch3x3'
     num_upsampling_layers = 'normal'
     phase = 'test'
-    prd_resize = 256
-    #preprocess_mode = 'resize_and_crop'
-    serial_batches = True
     use_vae = False
     z_dim = 256
