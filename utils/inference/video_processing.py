734f78bc95470baaf5290aba5628164f2ec94442
AlexanderGroshev <Nnn.alg@yandex.ru>
2022-03-21 16:27:49 +0300

    Smooth detection (#27)
    * update
    
    * minor fix
    
    * smooth detection
    
    * smooth detection
    
    Co-authored-by: danyache <dchesakov@nes.ru>

--- utils/inference/video_processing.py
+++ utils/inference/video_processing.py
@@ -5,6 +5,8 @@ from PIL import Image
 from typing import List, Tuple, Callable, Any
 from tqdm import tqdm
 import traceback
+from insightface.utils import face_align
+from scipy.spatial import distance
 
 from .masks import face_mask_static
 from .image_processing import normalize_and_torch, normalize_and_torch_batch, crop_face
@@ -26,8 +28,8 @@ def add_audio_from_another_video(video_with_sound: str,
         os.makedirs('./examples/audio/')
     fast_cmd = "-c:v libx264 -preset ultrafast -crf 18" if fast_cpu else ""
     gpu_cmd = "-c:v h264_nvenc" if gpu else ""
-    os.system(f"ffmpeg -i {video_with_sound} -vn -vcodec h264_nvenc ./examples/audio/{audio_name}.m4a")
-    os.system(f"ffmpeg -i {video_without_sound} -i ./examples/audio/{audio_name}.m4a {fast_cmd} {gpu_cmd}{video_without_sound[:-4]}_audio.mp4 -y")
+    os.system(f"ffmpeg -v -8 -i {video_with_sound} -vn -vcodec h264_nvenc ./examples/audio/{audio_name}.m4a")
+    os.system(f"ffmpeg -v -8 -i {video_without_sound} -i ./examples/audio/{audio_name}.m4a {fast_cmd} {gpu_cmd}{video_without_sound[:-4]}_audio.mp4 -y")
     os.system(f"rm -rf ./examples/audio/{audio_name}.m4a")
     os.system(f"mv {video_without_sound[:-4]}_audio.mp4 {video_without_sound}")
     
@@ -81,6 +83,31 @@ def get_target(full_frames: List[np.ndarray],
     return target
 
 
+def smooth_landmarks(kps_arr, n = 2):
+    kps_arr_smooth_final = []
+    for ka in kps_arr:
+        kps_arr_s = [[ka[0]]]
+        for i in range(1, len(ka)):
+            if (len(ka[i])==0) or (len(ka[i-1])==0):
+                kps_arr_s.append([ka[i]])
+            elif (distance.euclidean(ka[i][0], ka[i-1][0]) > 5) or (distance.euclidean(ka[i][2], ka[i-1][2]) > 5):
+                kps_arr_s.append([ka[i]])
+            else:
+                kps_arr_s[-1].append(ka[i])
+
+        kps_arr_smooth = []
+
+        for a in kps_arr_s:
+            a_smooth = []
+            for i in range(len(a)):
+                q = min(i-0, len(a)-i-1, n)      
+                a_smooth.append(np.mean( np.array(a[i-q:i+1+q]), axis=0 ) )
+
+            kps_arr_smooth += a_smooth
+        kps_arr_smooth_final.append(kps_arr_smooth)      
+    return kps_arr_smooth_final
+
+
 def crop_frames_and_get_transforms(full_frames: List[np.ndarray],
                 target_embeds: List,
                 app: Callable, 
@@ -94,12 +121,19 @@ def crop_frames_and_get_transforms(full_frames: List[np.ndarray],
     
     crop_frames = [ [] for _ in range(target_embeds.shape[0]) ]
     tfm_array = [ [] for _ in range(target_embeds.shape[0]) ]
+    kps_array = [ [] for _ in range(target_embeds.shape[0]) ]
     
     target_embeds = F.normalize(target_embeds)
     for frame in tqdm(full_frames):
         try:
-            faces, tfms = app.get(frame, crop_size)
-            if len(faces) > 1 or set_target:
+            kps = app.get(frame, crop_size)
+            if len(kps) > 1 or set_target:
+                faces = []
+                for p in kps:
+                    M, _ = face_align.estimate_norm(p, crop_size, mode ='None') 
+                    align_img = cv2.warpAffine(frame, M, (crop_size, crop_size), borderValue=0.0)
+                    faces.append(align_img)    
+                
                 face_norm = normalize_and_torch_batch(np.array(faces))
                 face_norm = F.interpolate(face_norm, scale_factor=0.5, mode='bilinear', align_corners=True)
                 face_embeds = netArc(face_norm)
@@ -109,18 +143,30 @@ def crop_frames_and_get_transforms(full_frames: List[np.ndarray],
                 best_idxs = similarity.argmax(0).detach().cpu().numpy()
                 for idx, best_idx in enumerate(best_idxs):
                     if similarity[best_idx][idx] > similarity_th:
-                        crop_frames[idx].append(faces[best_idx])
-                        tfm_array[idx].append(tfms[best_idx])
+                        kps_array[idx].append(kps[best_idx])
                     else:
-                        crop_frames[idx].append([])
-                        tfm_array[idx].append([])
+                        kps_array[idx].append([])
+
             else:
-                crop_frames[0].append(faces[0])
-                tfm_array[0].append(tfms[0])
+                kps_array[0].append(kps[0])     
+            
         except TypeError:
             for q in range (len(target_embeds)):                
+                kps_array[0].append([])
+        
+    smooth_kps = smooth_landmarks(kps_array, n = 2)
+                
+    for i, frame in tqdm(enumerate(full_frames)):
+        for q in range (len(target_embeds)):  
+            try:
+                M, _ = face_align.estimate_norm(smooth_kps[q][i], crop_size, mode ='None') 
+                align_img = cv2.warpAffine(frame, M, (crop_size, crop_size), borderValue=0.0)
+                crop_frames[q].append(align_img)
+                tfm_array[q].append(M)
+            except:             
                 crop_frames[q].append([])
-                tfm_array[q].append([])
+                tfm_array[q].append([])     
+                
     torch.cuda.empty_cache()
     return crop_frames, tfm_array
 
@@ -153,7 +199,7 @@ def get_final_video(final_frames: List[np.ndarray],
     Create final video from frames
     """
 
-    out = cv2.VideoWriter(f"{OUT_VIDEO_NAME}", cv2.VideoWriter_fourcc(*'MP4V'), fps, (full_frames[0].shape[1], full_frames[0].shape[0]))
+    out = cv2.VideoWriter(f"{OUT_VIDEO_NAME}", cv2.VideoWriter_fourcc(*'mp4v'), fps, (full_frames[0].shape[1], full_frames[0].shape[0]))
     size = (full_frames[0].shape[0], full_frames[0].shape[1])
     params = [None for i in range(len(crop_frames))]
     result_frames = full_frames.copy()
